40c6701f9e097867028f747cd9af7051d90b12e4
Blake Rose <bdecktor@gmail.com>
2019-05-05 21:38:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,4 +7,13 @@ def oxford_comma(array)
 end
 
 def oxford_comma(array)
-  array.join(" and ") 
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
+end
